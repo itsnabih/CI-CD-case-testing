2cc5a712743355da611e243e650c090d79059ed8
itsnabih <clayynh@gmail.com>
2024-09-17 22:01:55 +0700

    3.0 Update at test

--- index.test.js
+++ index.test.js
@@ -13,25 +13,25 @@ describe("GET /", () => {
   });
 });
 
-app.get("/health", (req, res) => {
-  res.send("halo, the endpoint is healthy!!!! (second update)");
-});
-describe("GET /health", () => {
-  it("should return healthy", async () => {
-    const rest = await request(app).get("/health");
-    expect(rest.text).toBe("halo, the endpoint is healthy!!!! (second update)");
-    expect(rest.statusCode).toBe(200);
-  });
-});
+// app.get("/health", (req, res) => {
+//   res.send("halo, the endpoint is healthy!!!! (second update)");
+// });
+// describe("GET /health", () => {
+//   it("should return healthy", async () => {
+//     const rest = await request(app).get("/health");
+//     expect(rest.text).toBe("halo, the endpoint is healthy!!!! (second update)");
+//     expect(rest.statusCode).toBe(200);
+//   });
+// });
 
-app.get("/ci-cd", (req, res) => {
-  res.send("ci-cd is working fine!!");
-});
+// app.get("/ci-cd", (req, res) => {
+//   res.send("ci-cd is working fine!!");
+// });
 
-describe("GET /ci-cd", () => {
-  it("should return ci-cd page working", async () => {
-    const rest = await request(app).get("/ci-cd");
-    expect(rest.text).toBe("ci-cd is working fine!!");
-    expect(rest.statusCode).toBe(200);
-  });
-});
+// describe("GET /ci-cd", () => {
+//   it("should return ci-cd page working", async () => {
+//     const rest = await request(app).get("/ci-cd");
+//     expect(rest.text).toBe("ci-cd is working fine!!");
+//     expect(rest.statusCode).toBe(200);
+//   });
+// });
